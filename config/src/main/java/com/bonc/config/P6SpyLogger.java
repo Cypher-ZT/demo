ac77a6724f23ea34bbbf9a34387f3a24e0325fbd
yanzening <1223438828@qq.com>
2020-06-16 17:54:25 +0800

    111

---
+++ config/src/main/java/com/bonc/config/P6SpyLogger.java
@@ -0,0 +1,21 @@
+package com.bonc.config;
+
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * description:
+ *
+ * @author wkGui
+ */
+public class P6SpyLogger implements MessageFormattingStrategy {
+    private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql,String url) {
+        return !"".equals(sql.trim()) ? this.format.format(new Date()) + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + "\n " + sql + ";" : "";
+    }
+
+}
